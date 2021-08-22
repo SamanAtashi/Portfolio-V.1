b2c669c07c19af35440efce9c8b4965fd3df5e51
SamanAtashi <milady00126@gmail.com>
2021-08-22 21:35:05 +0300

    Add sass to webpack

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import _ from 'lodash';
-import './style.css';
+import './sass/style.scss';
 
 function component() {
 	const element = document.createElement('div');
-    
+
 	// Lodash, currently included via a script, is required for this line to work
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add("hello");
+	element.classList.add('hello');
 	return element;
 }
 
