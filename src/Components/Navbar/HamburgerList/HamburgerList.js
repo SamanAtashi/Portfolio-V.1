5737372cee878b67311d645168583f95f4e8d286
SamanAtashi <milady00126@gmail.com>
2021-09-21 10:15:20 +0300

    Add styles-components

--- src/Components/Navbar/HamburgerList/HamburgerList.js
+++ src/Components/Navbar/HamburgerList/HamburgerList.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
+import './HamburgerList.module.css';
+
 const HamburgerList = (props) => {
-	return <ul>hamburger/list of items</ul>;
+	return (
+		<ul>
+			<li>Home</li>
+			<li>About</li>
+			<li>Projects</li>
+			<li>Contact</li>
+		</ul>
+	);
 };
 export default HamburgerList;
