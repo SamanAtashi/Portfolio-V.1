c3b4efcd65d554608fb83030f9aba002b880b046
SamanAtashi <milady00126@gmail.com>
2021-09-24 14:04:51 +0300

    Remove unnecessar stuff from navbar i will comback to them later

--- src/Components/Navbar/HamburgerList/HamburgerList.js
+++ src/Components/Navbar/HamburgerList/HamburgerList.js
@@ -5,27 +5,10 @@ import List from './List/List';
 import './HamburgerList.module.css';
 
 class HamburgerList extends React.Component {
-	state = {
-		open: false,
-	};
-
-	openHandler = () => {
-		this.setState((preOpen) => {
-			console.log(preOpen);
-			return { open: !preOpen };
-		});
-		console.log(this.state.open);
-	};
-
-	consol = () => {
-		console.log(this.state.open);
-	};
-
 	render() {
 		return (
 			<>
-				<Hamburger openHandler={this.openHandler} />
-				<button onClick={this.consol}>console</button>
+				<Hamburger />
 				<List />
 			</>
 		);
