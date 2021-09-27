00fb4686787be3ce0a1bbf7fa4ad5ef99ce92639
SamanAtashi <milady00126@gmail.com>
2021-09-27 11:56:55 +0300

    Fix nav hamburger

--- src/Components/Navbar/HamburgerList/HamburgerList.js
+++ src/Components/Navbar/HamburgerList/HamburgerList.js
@@ -5,10 +5,24 @@ import List from './List/List';
 import './HamburgerList.module.css';
 
 class HamburgerList extends React.Component {
+	state = {
+		showList: false,
+	};
+
+	hamOpenHandler = () => {
+		this.setState((preState) => {
+			return { showList: !preState.showList };
+		});
+		console.log(this.state.showList);
+	};
+
 	render() {
 		return (
 			<>
-				<Hamburger />
+				<Hamburger
+					openHandler={this.hamOpenHandler}
+					showList={this.state.showList}
+				/>
 				<List />
 			</>
 		);
