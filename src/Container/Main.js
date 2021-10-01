cb70032887399fc687fdd16b124ffbc0f427ecfe
SamanAtashi <milady00126@gmail.com>
2021-10-01 11:07:37 +0300

    Add component and a little style to Home page

--- src/Container/Main.js
+++ src/Container/Main.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Home from '../Components/Home/Home';
 import Navbar from '../Components/Navbar/Navbar';
 
 const Main = () => {
 	return (
 		<>
 			<Navbar />
-			<main>Home</main>
+			<main>
+				<Home />
+			</main>
 			<footer>all the rights reserved</footer>
 		</>
 	);
