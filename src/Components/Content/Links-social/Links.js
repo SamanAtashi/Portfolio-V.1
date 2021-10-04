82b2a9be9576b60f44b1cbaff6007a2601cccb3f
SamanAtashi <milady00126@gmail.com>
2021-10-04 08:36:19 +0300

    Add linkedin icon

---
+++ src/Components/Content/Links-social/Links.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FaLinkedin } from 'react-icons/fa';
+
+const Links = () => {
+	return (
+		<div>
+			<FaLinkedin />
+		</div>
+	);
+};
+
+export default Links;
--- src/Container/GlassLinks/GlassLinks.js
+++ src/Container/GlassLinks/GlassLinks.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Links from '../../Components/Content/Links-social/Links';
 import classes from './GlassLinks.module.css';
 
 const GlassLinks = () => {
-	return <div className={classes.glass}></div>;
+	return (
+		<div className={classes.glass}>
+			<Links />
+		</div>
+	);
 };
 
 export default GlassLinks;
