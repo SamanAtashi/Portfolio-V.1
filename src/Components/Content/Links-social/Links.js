335615f197fd2b7ee8f47eba4e4b308b7d63f365
SamanAtashi <milady00126@gmail.com>
2021-10-04 09:37:22 +0300

    Add react icons

--- src/Components/Content/Links-social/Links.js
+++ src/Components/Content/Links-social/Links.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { FaLinkedin } from 'react-icons/fa';
+import { BsGithub } from 'react-icons/bs';
+// import { AiOutlineMail } from 'react-icons/ai';
+import { MdEmail } from 'react-icons/md';
+import { BsTwitter } from 'react-icons/bs';
 
 const Links = () => {
 	return (
 		<div>
 			<FaLinkedin />
+			<BsGithub />
+			{/* <AiOutlineMail /> */}
+			<MdEmail />
+			<BsTwitter />
 		</div>
 	);
 };
