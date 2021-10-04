301480c93622939af44daf188b9aaff7e3555d07
SamanAtashi <milady00126@gmail.com>
2021-10-04 09:47:56 +0300

    Add hover color and size

--- src/Components/Content/Links-social/Links.js
+++ src/Components/Content/Links-social/Links.js
@@ -4,10 +4,11 @@ import { BsGithub } from 'react-icons/bs';
 // import { AiOutlineMail } from 'react-icons/ai';
 import { MdEmail } from 'react-icons/md';
 import { BsTwitter } from 'react-icons/bs';
+import classes from './Links.module.css';
 
 const Links = () => {
 	return (
-		<div>
+		<div className={classes.icons}>
 			<FaLinkedin />
 			<BsGithub />
 			{/* <AiOutlineMail /> */}
