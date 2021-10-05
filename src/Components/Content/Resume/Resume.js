1bbda6bd99a79b389bb8896a1f1c05bf5e24d5db
SamanAtashi <milady00126@gmail.com>
2021-10-05 10:59:45 +0300

    Update resume btn

--- src/Components/Content/Resume/Resume.js
+++ src/Components/Content/Resume/Resume.js
@@ -3,8 +3,9 @@ import classes from './Resume.module.css';
 
 const Resume = () => {
 	return (
-		<button className={`${classes.gradientBorder} ${classes.btn}`}>
-			Resume
+		// <button className={`${classes.gradientBorder} ${classes.btn}`}>
+		<button className={classes.horizontal}>
+			<span className={classes.text}>Resume</span>
 		</button>
 	);
 };
