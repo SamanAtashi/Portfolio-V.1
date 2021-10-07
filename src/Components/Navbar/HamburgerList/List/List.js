1f67193e3ea5b34e0127cefb386d7713447c11f9
SamanAtashi <milady00126@gmail.com>
2021-10-07 11:18:11 +0300

    Delete middle link

--- src/Components/Navbar/HamburgerList/List/List.js
+++ src/Components/Navbar/HamburgerList/List/List.js
@@ -53,15 +53,6 @@ const List = () => {
 			<li className={classes.list_item} onClick={clickedHandler}>
 				<HashLink
 					smooth
-					to="#middle"
-					className={classes.link_item}
-				>
-					middle
-				</HashLink>
-			</li>
-			<li className={classes.list_item} onClick={clickedHandler}>
-				<HashLink
-					smooth
 					to="#Projects"
 					className={classes.link_item}
 				>
