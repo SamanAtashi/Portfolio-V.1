ee134b493bf9371185372f306ca39cc2f8321fed
SamanAtashi <milady00126@gmail.com>
2021-10-08 09:36:22 +0300

    Add Project and state to projects

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import Project from './Project/Project';
 
-const Projects = () => {
-	return <p id="Projects">Projects</p>;
-};
+class Projects extends React.Component {
+	state = {
+		project_1: {
+			name: 'sth',
+			id: 'num1',
+			img_src: '',
+			description: '',
+		},
+	};
+
+	render() {
+		return (
+			<div id="Projects">
+				<Project />
+			</div>
+		);
+	}
+}
 
 export default Projects;
