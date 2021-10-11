602a61d635fce0ac6a6a11300cfc89ec6c5b1327
SamanAtashi <milady00126@gmail.com>
2021-10-11 15:14:34 +0300

    Update size of about and home page for bigger screens

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -14,35 +14,42 @@ const About = () => {
 					complex world.
 				</p>
 			</article>
-			<article className={classes.skills}>
-				<header>Skills</header>
-				<ul>
-					<li>JavaScript</li>
-					<li>React,Redux</li>
-					<li>HTML,CSS</li>
-					<li>Ruby</li>
-					<li>Git,Github</li>
-					<li>TDD,RSpec</li>
-					<li>Responsive Design</li>
-				</ul>
-			</article>
-			<article className={classes.exp}>
-				<header>Experiences & Education</header>
-				<ul>
-					<li>
-						<div>
-							<p>Student at:</p>
-							<a
-								href="https://www.microverse.org/"
-								className={classes.companies}
-							>
-								Microverse
-							</a>
-						</div>
-						<p className={classes.date}> March 2021 - Present</p>
-					</li>
-				</ul>
-			</article>
+			<div className={classes.screen}>
+				<article className={classes.skills}>
+					<header>Skills</header>
+					<ul>
+						<li>JavaScript</li>
+						<li>React,Redux</li>
+						<li>HTML,CSS</li>
+						<li>Ruby</li>
+						<li>Git,Github</li>
+						<li>TDD,RSpec</li>
+						<li>Responsive Design</li>
+					</ul>
+				</article>
+				<article className={classes.exp}>
+					<header>Experiences & Education</header>
+					<ul>
+						<li>
+							<div>
+								<p>Student at:</p>
+								<a
+									href="https://www.microverse.org/"
+									className={
+										classes.companies
+									}
+								>
+									Microverse
+								</a>
+							</div>
+							<p className={classes.date}>
+								{' '}
+								March 2021 - Present
+							</p>
+						</li>
+					</ul>
+				</article>
+			</div>
 		</section>
 	);
 };
