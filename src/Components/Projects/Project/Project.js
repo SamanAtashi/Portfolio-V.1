7119266e0607b37302ff23d1b086d296ffd5d377
SamanAtashi <milady00126@gmail.com>
2021-10-12 09:26:57 +0300

    Add p tag of coming soon to projects

--- src/Components/Projects/Project/Project.js
+++ src/Components/Projects/Project/Project.js
@@ -6,18 +6,23 @@ const Article = styled.article`
 	position: relative;
 `;
 
-const Description = styled.p`
+const Description = styled.div`
 	display: none;
 	${Article}:hover & {
 		border-radius: 1.2rem;
-		display: block;
+		display: flex;
+		justify-content: center;
+		align-items: center;
 		position: absolute;
 		top: 0;
 		left: 0;
 		width: 100%;
 		height: 100%;
-		color: var(--main-1-color);
+		text-align: center;
+		vertical-align: middle;
+		color: var(--main-2-color);
 		background: var(--main-3-color);
+		z-index: 9;
 	}
 `;
 
@@ -26,7 +31,11 @@ const Project = (props) => {
 		<Article className={classes.container}>
 			<header>{props.name}</header>
 			<Description className={classes.scaleIn}>
-				{props.desc}
+				<p>
+					{props.status === 'Coming Soon...'
+						? 'Coming Soon...'
+						: props.desc}
+				</p>
 			</Description>
 			<div className={classes.img}>
 				{props.status === 'Coming Soon...' ? (
