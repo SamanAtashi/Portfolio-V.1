85cffdbc912dd7c613642d4fe72cd28e87722b37
SamanAtashi <milady00126@gmail.com>
2021-10-12 16:09:12 +0300

    Add footer

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './Footer.module.css';
+
+const Footer = () => {
+	return (
+		<footer className={classes.container}>
+			<p>&copy; 2021 Saman Atashi</p>
+		</footer>
+	);
+};
+export default Footer;
--- src/Container/Main.js
+++ src/Container/Main.js
@@ -10,6 +10,7 @@ import smoothScrollPolyfill from 'smoothscroll-polyfill';
 
 import About from '../Components/About/About';
 import Contact from '../Components/Contact/Contact';
+import Footer from '../Components/Footer/Footer';
 import Home from '../Components/Home/Home';
 import Navbar from '../Components/Navbar/Navbar';
 import Projects from '../Components/Projects/Projects';
@@ -55,14 +56,7 @@ const Main = () => {
 		<Router>
 			<Navbar />
 			<main>{right}</main>
-			<footer
-				style={{
-					marginTop: '2rem',
-					// borderTop: '1px solid red',
-				}}
-			>
-				all the rights reserved
-			</footer>
+			<Footer />
 			<GlassLinks />
 		</Router>
 	);
