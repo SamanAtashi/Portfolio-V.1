5283bdb4b6c04422e4a5e3193c0cb1ef9a37282d
SamanAtashi <milady00126@gmail.com>
2021-10-14 17:30:46 +0300

    add project 4

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -4,6 +4,7 @@ import classes from './Projects.module.css';
 import nyct from '../../Assets/images/nyct.png';
 import newsweek from '../../Assets/images/newsweek.png';
 import TicTacToe from '../../Assets/images/ttt.png';
+import Awesome_Books from '../../Assets/images/ab.png';
 class Projects extends React.Component {
 	state = {
 		project_1: {
@@ -30,7 +31,7 @@ class Projects extends React.Component {
 				'https://github.com/SamanAtashi/Newsweek_clone_page',
 		},
 		project_3: {
-			name: 'Ruby-TicTacToe',
+			name: 'TicTacToe (Ruby)',
 			status: 'Done',
 			id: 'num3',
 			img_src: TicTacToe,
@@ -39,6 +40,17 @@ class Projects extends React.Component {
 			stack: ['Ruby'],
 			url: 'https://github.com/SamanAtashi/Tic_Tac_Toe',
 			githubUrl: 'https://github.com/SamanAtashi/Tic_Tac_Toe',
+		},
+		project_4: {
+			name: 'Awesome Books',
+			status: 'Done',
+			id: 'num4',
+			img_src: Awesome_Books,
+			description:
+				'Awesome Library is a mini book library project tha allows a user to add books, and remove a book. The project works with `localStorage` for its backend.',
+			stack: ['JS', 'HTML', 'CSS'],
+			url: 'https://samanatashi.github.io/Awesome_Books/',
+			githubUrl: 'https://github.com/SamanAtashi/Awesome_Books',
 		},
 		// !-->coming soon
 		project: {
@@ -76,6 +88,15 @@ class Projects extends React.Component {
 					stack={this.state.project_3.stack}
 					url={this.state.project_3.url}
 					githubUrl={this.state.project_3.githubUrl}
+				/>
+				<Project
+					name={this.state.project_4.name}
+					desc={this.state.project_4.description}
+					status={this.state.project_4.status}
+					src={this.state.project_4.img_src}
+					stack={this.state.project_4.stack}
+					url={this.state.project_4.url}
+					githubUrl={this.state.project_4.githubUrl}
 				/>
 				{/* <Project
 					name={this.state.project_4.name}
