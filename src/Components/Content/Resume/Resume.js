a9f07e44bc7b51cedff603c4c734fd95f7eed2a7
SamanAtashi <milady00126@gmail.com>
2021-10-15 14:10:54 +0300

    Add pdf resume

--- src/Components/Content/Resume/Resume.js
+++ src/Components/Content/Resume/Resume.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import classes from './Resume.module.css';
+import resume from '../../../Assets/Resume/resume.pdf';
 
 const Resume = () => {
 	return (
 		// <button className={`${classes.gradientBorder} ${classes.btn}`}>
 		<button className={classes.horizontal}>
-			<span className={classes.text}>Resume</span>
+			<a href={resume} className={classes.text} download>
+				Resume
+			</a>
 		</button>
 	);
 };
